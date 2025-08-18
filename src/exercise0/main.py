744591176f7b31abeea892659f2378249b262ae1
Крохмалев Константин Олегович <konstantin.krohmalev@nami.ru>
2025-08-18 18:28:04 +0300

    fix table ex0 work

--- src/exercise0/main.py
+++ src/exercise0/main.py
@@ -5,7 +5,7 @@ import random
 from prettytable import PrettyTable
 from models.entities import Examiner, Student, Question
 from models.examiner_logic import run_exam
-from visuals import update_display, clear_console
+from visuals import update_display, render_final_report
 from analytics import (
     best_student,
     best_examiner_names,
@@ -58,33 +58,18 @@ def main():
         t.join()
     exam_finished_event.set()
     display_thread.join()
-
-    clear_console()
-
-    student_table = PrettyTable()
-    student_table.field_names = ["Студент", "Статус"]
-    for s in students:
-        if s.status == 'Сдал':
-            student_table.add_row([s.name, s.status])
-
-    for s in students:
-        if s.status == 'Провалил':
-            student_table.add_row([s.name, s.status])
-    print(student_table)
-
-    examiner_table = PrettyTable()
-    examiner_table.field_names = ["Экзаменатор", "Всего студентов", "Завалил", "Время работы"]
-    for e in examiners:
-        examiner_table.add_row([e.name, e.students_handled, e.failed, f"{e.work_time:.2f}"])
-    print(examiner_table)
-
-    total_time = time.time() - start_time   
-    print(f"Время с момента начала экзамена и до момента и его завершения: {total_time:.2f}")
-    print(f"Имена лучших студентов: {best_student(students)}")
-    print(f"Имя лучших экзаменаторов: {best_examiner_names(examiners)}")
-    print(f"Имена студентов, которых после экзамена отчислят: {student_expelled(students)}")
-    print(f"Лучшие вопросы: {best_questions(questions)}")
-    print(f"Вывод: {exam_status(students)}")
+    
+    render_final_report(
+        students=students,
+        examiners=examiners,
+        questions=questions,
+        start_time=start_time,
+        best_student=best_student,
+        best_examiner_names=best_examiner_names,
+        student_expelled=student_expelled,
+        best_questions=best_questions,
+        exam_status=exam_status,
+    )
 
 
 if __name__=="__main__":
--- src/exercise0/visuals.py
+++ src/exercise0/visuals.py
@@ -39,3 +39,36 @@ def update_display(students, examiners, student_queue, start_time, original_orde
         draw_state(students, examiners, student_queue, start_time, original_order)
         time.sleep(0.2)
         # time.sleep(3000000)
+
+def render_final_report(students, examiners, questions, start_time,
+                        best_student, best_examiner_names, student_expelled,
+                        best_questions, exam_status):
+    clear_console()
+
+     # --- Таблица студентов ---
+    student_table = PrettyTable()
+    student_table.field_names = ["Студент", "Статус"]
+    for s in students:
+        if s.status == 'Сдал':
+            student_table.add_row([s.name, s.status])
+
+    for s in students:
+        if s.status == 'Провалил':
+            student_table.add_row([s.name, s.status])
+    print(student_table)
+
+    # --- Таблица экзаменаторов ---
+    examiner_table = PrettyTable()
+    examiner_table.field_names = ["Экзаменатор", "Всего студентов", "Завалил", "Время работы"]
+    for e in examiners:
+        examiner_table.add_row([e.name, e.students_handled, e.failed, f"{e.work_time:.2f}"])
+    print(examiner_table)
+
+    # --- Метрики/итоги ---
+    total_time = time.time() - start_time   
+    print(f"Время с момента начала экзамена и до момента и его завершения: {total_time:.2f}")
+    print(f"Имена лучших студентов: {best_student(students)}")
+    print(f"Имя лучших экзаменаторов: {best_examiner_names(examiners)}")
+    print(f"Имена студентов, которых после экзамена отчислят: {student_expelled(students)}")
+    print(f"Лучшие вопросы: {best_questions(questions)}")
+    print(f"Вывод: {exam_status(students)}")
